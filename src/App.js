2f48c2b4721a0ffd40dd578e23fe9499385eb5ce
Brian Li <antai0926@gmail.com>
2020-08-10 14:34:44 +0800

    fetch posts data and create basic view template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import UserProfile from './component/user-profile.component';
 
--- src/component/user-profile.component.jsx
+++ src/component/user-profile.component.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import './user-profile.styles.sass';
+import React, { useEffect, useState } from 'react';
+import './user-profile.styles.scss';
 
 const UserProfile = (props) => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setPosts(data.slice(0, 3)));
+  }, []);
+
   return (
-    <div className="user-profile">
+    <div className="container">
       <h1 className="name"> {props.name} </h1>
-      <h1 className="email"> {props.email} </h1>
+      <h2 className="email"> {props.email} </h2>
+      <h3>Post Counts: {posts.length}</h3>
+      posts:
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
